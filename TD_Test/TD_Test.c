a7270997c63528733ccbc55bad3851f68aa3ead5
Christopher Moore <51140505+chmoore889@users.noreply.github.com>
2024-05-02 15:50:54 -0400

    More testing code

--- TD_Test/TD_Test.c
+++ TD_Test/TD_Test.c
@@ -3,9 +3,44 @@
 
 int main() {
     void* tagger = getTagger();
-    printf("Got Time Tagger %p", tagger);
-    if (tagger != NULL) {
-        freeTagger(tagger);
+    printf("Got Time Tagger %p\n", tagger);
+    if (tagger == NULL) {
+        return 1;
     }
+
+    int channels[] = {2};
+    void* measurement = newMeasurement(tagger, (MeasurementParams_t) {
+        .laserChannel = -1,
+        .laserTriggerVoltage = -0.5,
+        .laserPeriod = 12500,
+        .detectorChannels = channels,
+        .detectorChannelsLength = sizeof(channels) / sizeof(*channels),
+        .detectorTriggerVoltage = 0.9,
+    });
+    if (measurement == NULL) {
+        freeTagger(tagger);
+        return 1;
+    }
+
+    printf("Starting measurement\n");
+    startMeasurement(measurement);
+    printf("Measurement started\n");
+    
+    while (getchar() != '\n') {
+        MacroMicro_t* data;
+        size_t dataSize;
+        int ret = getData(measurement, &data, &dataSize);
+        if(ret != 0) {
+			printf("Error getting data\n");
+			break;
+		}
+    }
+
+    printf("Stopping measurement\n");
+    stopMeasurement(measurement);
+
+    printf("Freeing taggers\n");
+
+    freeTagger(tagger);
     return 0;
 }
